3446340f1368a93c90c29d27b1903522ba190a6b
Nived P A <62166124+nivedwho@users.noreply.github.com>
2021-10-05 09:08:46 +0530

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -16,7 +16,6 @@ def is_image_file(filename):
 
 def make_dataset(dir):
     images = []
-    dir = '/content/imaget1/'
     assert os.path.isdir(dir), '%s is not a valid directory' % dir
     for root, _, fnames in sorted(os.walk(dir)):
         for fname in fnames:
